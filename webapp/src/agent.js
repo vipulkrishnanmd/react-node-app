99da7fdf6edce07a9416167e884f23f31c78e08d
Vipul Krishnan <vkrishnan@freelancer.com>
2020-05-04 23:51:36 +1000

    changing client to server url

--- webapp/src/agent.js
+++ webapp/src/agent.js
@@ -3,7 +3,7 @@ import _superagent from "superagent";
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = "http://localhost:3000";
+const API_ROOT = "http://server:3000";
 
 const encode = encodeURIComponent;
 const responseBody = (res) => res.body;
