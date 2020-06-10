60b78649ac19309a64ecc5e172ee097ea0ec5aa7
Vipul Krishnan <vkrishnan@freelancer.com>
2020-06-10 10:24:33 +1000

    fix

--- webapp/src/agent.js
+++ webapp/src/agent.js
@@ -3,7 +3,7 @@ import _superagent from "superagent";
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = location.protocol+'//'+location.hostname+":3000/api"; // TODO: change it to /api for kube
+const API_ROOT = window.location.protocol+'//'+ window.location.hostname+":3000/api"; // TODO: change it to /api for kube
 
 const encode = encodeURIComponent;
 const responseBody = (res) => res.body;
